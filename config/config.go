9f0cbf0b41f88f931c463e155322077eaa04306a
golovanevvs <lony@inbox.ru>
2025-07-19 18:18:17 +0300

    Feat: config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"strings"
+
 	"github.com/spf13/viper"
 )
 
@@ -10,14 +12,32 @@ type Config struct {
 
 func New() *Config {
 	v := viper.New()
+
+	v.AutomaticEnv()
+
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
 	return &Config{v: v}
+}
+
+func (c *Config) SetEnvPrefix(prefix string) {
+	c.v.SetEnvPrefix(prefix)
 }
 
 func (c *Config) Load(path string) error {
 	c.v.SetConfigFile(path)
+
 	return c.v.ReadInConfig()
 }
 
 func (c *Config) GetString(key string) string {
 	return c.v.GetString(key)
+}
+
+func (c *Config) GetInt(key string) int {
+	return c.v.GetInt(key)
+}
+
+func (c *Config) GetBool(key string) bool {
+	return c.v.GetBool(key)
 }
