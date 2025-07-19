951a08771656861d259b5ac2fd6e98e92ab92afc
golovanevvs <lony@inbox.ru>
2025-07-19 22:00:51 +0300

    Feat: zerolog

--- zlog/zlog.go
+++ zlog/zlog.go
@@ -9,13 +9,6 @@ import (
 var Logger zerolog.Logger
 
 func Init() {
-	// consoleWriter := zerolog.ConsoleWriter{
-	// 	Out:        os.Stdout,
-	// 	TimeFormat: "2006-01-02 15:04:05",
-	// 	FormatLevel: func(i interface{}) string {
-	// 		return colorizeLevel(i.(string))
-	// 	},
-	// }
 	Logger = zerolog.New(os.Stdout).
 		With().
 		Timestamp().
@@ -26,18 +19,3 @@ func Init() {
 func ParseLogLevel(level string) (zerolog.Level, error) {
 	return zerolog.ParseLevel(level)
 }
-
-// func colorizeLevel(level string) string {
-// 	switch level {
-// 	case "debug":
-// 		return "\x1b[36m" + level + "\x1b[0m" // Голубой
-// 	case "info":
-// 		return "\x1b[32m" + level + "\x1b[0m" // Зелёный
-// 	case "warn":
-// 		return "\x1b[33m" + level + "\x1b[0m" // Жёлтый
-// 	case "error", "fatal", "panic":
-// 		return "\x1b[31m" + level + "\x1b[0m" // Красный
-// 	default:
-// 		return level
-// 	}
-// }
