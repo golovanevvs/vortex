04b5410e31126fe68ba6db8e7a537523e2627ac8
golovanevvs <lony@inbox.ru>
2025-07-20 15:44:31 +0300

    Feat: ginext: add WithLogging

--- ginext/ginext.go
+++ ginext/ginext.go
@@ -1,11 +1,22 @@
 package ginext
 
 import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
 )
 
 type Engine struct {
 	*gin.Engine
+}
+
+type responseBody struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
 }
 
 type Context = gin.Context
@@ -20,4 +31,86 @@ func (e *Engine) GET(relativePath string, handlers ...gin.HandlerFunc) {
 
 func (e *Engine) Run(addr ...string) error {
 	return e.Engine.Run(addr...)
+}
+
+func (e Engine) WithLogging(logger *zerolog.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		logLevel := logger.GetLevel()
+
+		if logLevel == zerolog.Disabled {
+			c.Next()
+			return
+		}
+
+		start := time.Now()
+
+		var writer *responseBody
+		if logLevel <= zerolog.DebugLevel {
+			writer = &responseBody{
+				ResponseWriter: c.Writer,
+				body:           bytes.NewBufferString(""),
+			}
+			c.Writer = writer
+		}
+
+		c.Next()
+
+		logCtx := logger.With().
+			Str("Request method", c.Request.Method).
+			Str("Request path", c.Request.URL.Path).
+			Str("Request query", c.Request.URL.RawQuery).
+			Str("Request ip", c.ClientIP()).
+			Str("Request Content-Type", c.ContentType()).
+			Str("Request user-agent", c.Request.UserAgent()).
+			Dur("latency", time.Since(start)).
+			Int("Response status", c.Writer.Status()).
+			Int("Response size", c.Writer.Size())
+
+		if logLevel <= zerolog.DebugLevel && writer != nil {
+			logCtx = logCtx.Str("Response body", writer.body.String())
+			if body := writer.body.String(); body != "" {
+				if strings.Contains(writer.Header().Get("Content-Type"), "application/json") {
+					var pretty bytes.Buffer
+					if err := json.Indent(&pretty, []byte(body), "", "  "); err != nil {
+						logCtx = logCtx.RawJSON("response", []byte(body))
+					} else {
+						logCtx = logCtx.RawJSON("response", []byte(pretty.Bytes()))
+					}
+				} else {
+					var truncateBody string
+					maxLenBody := 1024
+					if len(body) < maxLenBody {
+						truncateBody = body
+					} else {
+						truncateBody = body[:maxLenBody] + "...[truncated]"
+					}
+					logCtx = logCtx.Str("response", truncateBody)
+				}
+			}
+		}
+
+		log := logCtx.Logger()
+
+		msg := "Request handled"
+
+		if len(c.Errors) > 0 {
+			errors := make([]error, len(c.Errors))
+			for i, e := range c.Errors {
+				errors[i] = e.Err
+			}
+			log.Error().Errs("errors", errors).Msg(msg)
+		} else {
+			switch {
+			case logLevel <= zerolog.DebugLevel:
+				log.Debug().Msg(msg)
+			case logLevel <= zerolog.InfoLevel:
+				log.Info().Msg(msg)
+			}
+		}
+	}
+}
+
+func (w *responseBody) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
 }
